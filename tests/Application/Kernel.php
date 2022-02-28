99a0f3e2751e7cd0e858f01c5645274b7e172199
Julie <julie.nobody@gmail.com>
2022-02-28 15:25:06 +0100

    functional version PHP8.0 test ok

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -85,7 +85,7 @@ final class Kernel extends BaseKernel
 
     protected function getContainerBaseClass(): string
     {
-        if ($this->isTestEnvironment()) {
+        if ($this->isTestEnvironment() && class_exists(MockerContainer::class)) {
             return MockerContainer::class;
         }
 
